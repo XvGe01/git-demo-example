9f6d1fd8acad80995089abf313da493b9a6c640b
gitXv <1785473283@qq.com>
2022-03-21 08:41:07 +0800

    veggies order system

--- veggies.py
+++ veggies.py
@@ -1,2 +1,8 @@
-veggies = ['cabbage', 'carrot', 'spinach',
-'asparagus','artichoke', 'pumpkin','lettuce']
+veggies = {'cabbage':2.87, 'carrot':1.59, 'spinach':3.33,
+'asparagus':2.54,'artichoke':3.00,,'lettuce':2.43}
+order = {"cabbage":2,"artichoke":5}
+
+total = 0
+for veggie,quantity in order.items():
+      total += veg_price[veggie] * quantity
+print(total)
